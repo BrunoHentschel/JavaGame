3af0183d9bdec6f302740b44b943d078448b0138
BrunoHentschel <162595255+BrunoHentschel@users.noreply.github.com>
2024-03-08 00:27:20 +0100

    Hauptfenster erster Schritt #01 4:29

---
+++ Java2DGame/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Java2DGame");
+        window.setSize(1000, 600);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+
+}
